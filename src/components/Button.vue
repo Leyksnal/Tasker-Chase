c40d6d234f7055632d92588957e5af2ad186fb69
leyksnal <leyksnal@gmail.com>
2021-09-23 18:53:43 +0100

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+}
+</script>
+
+<style>
+.btn{
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+
+<script>
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    }
+}
+</script>
